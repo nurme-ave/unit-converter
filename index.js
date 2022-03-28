60501670c0fd26bf6fe2022a1cc640569182d195
Ave Nurme <nurme.ave@gmail.com>
2022-03-29 01:22:05 +0300

    Changes made with the roundNumber() function

--- index.js
+++ index.js
@@ -24,26 +24,32 @@ function displayResults() {
 }
 
 function convertLength(numberToConvert) {
-  const metersToFeet = (numberToConvert * 3.28084);
-  const feetToMeters = (numberToConvert / 3.28084);
-  const lengthResult = [roundNumber(metersToFeet), roundNumber(feetToMeters)];
+  const metersToFeet = numberToConvert * 3.28084;
+  const feetToMeters = numberToConvert / 3.28084;
+  const lengthResult = roundNumber(metersToFeet, feetToMeters);
   return lengthResult;
 }
 
 function convertVolume(numberToConvert) {
-  const litersToGallons = (numberToConvert * 0.2641);
-  const gallonsToLiters = (numberToConvert / 0.2641);
-  const volumeResult = [roundNumber(litersToGallons), roundNumber(gallonsToLiters)];
+  const litersToGallons = numberToConvert * 0.2641;
+  const gallonsToLiters = numberToConvert / 0.2641;
+  const volumeResult = roundNumber(litersToGallons, gallonsToLiters);
   return volumeResult;
 }
 
 function convertMass(numberToConvert) {
-  const kilostoPounds = (numberToConvert * 2.20462);
-  const poundsToKilos = (numberToConvert / 2.20462);
-  const massResult = [roundNumber(kilostoPounds), roundNumber(poundsToKilos)];
+  const kilostoPounds = numberToConvert * 2.20462;
+  const poundsToKilos = numberToConvert / 2.20462;
+  const massResult = roundNumber(kilostoPounds, poundsToKilos);
   return massResult;
 }
 
-function roundNumber(numberToRound) {
-  return +numberToRound.toFixed(3);
+/*
+  Pass in the array as a rest parameter and
+  apply toFixed()-method to each of the numbers
+  in the array.
+*/
+function roundNumber(...numbersToRound) {
+  const roundedNumbers = numbersToRound.map((number) => number.toFixed(3));
+  return roundedNumbers;
 }
